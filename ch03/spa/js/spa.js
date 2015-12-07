3a8bee87c7d11de0368a74f7bb916bd31dbd17ed
hackrole <daipeng123456@gmail.com>
2015-12-07 20:40:51 +0800

    finish hello world

---
+++ ch03/spa/js/spa.js
@@ -0,0 +1,14 @@
+/*
+ * spa.js
+ * root namespaces module
+ */
+
+var spa = (function() {
+  var initModule = function($container){
+    $container.html(
+      '<h1 style="display:inline-block; margin: 25px;">hello world!</h1>'
+    );
+  };
+
+  return {initModule: initModule};
+}());
