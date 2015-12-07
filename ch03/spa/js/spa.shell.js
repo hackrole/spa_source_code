5c6b3dfe4bf35721f3a1d959a208777242f94c43
hackrole <daipeng123456@gmail.com>
2015-12-07 21:26:49 +0800

    finish spa.shell.js

---
+++ ch03/spa/js/spa.shell.js
@@ -0,0 +1,43 @@
+/*
+ * spa.shell.js
+ * shell module for spa
+ */
+
+spa.shell = (function(){
+
+  var configMap = {
+    main_html: String() +
+      + '<div class="spa-shell-head">'
+        + '<div class="spa-shell-head-logo"></div>'
+        + '<div class="spa-shell-head-acct"></div>'
+        + '<div class="spa-shell-head-search"></div>'
+      + '</div>'
+      + '<div class="spa-shell-main">'
+        + '<div class="spa-shell-main-nav"></div>'
+        + '<div class="spa-shell-main-content"></div>'
+      + '</div>'
+      + '<div class="spa-shell-foot"></div>'
+      + '<div class="spa-shell-chat"></div>'
+      + '<div class="spa-shell-modal"></div>'
+  };
+
+  var stateMap = {
+    $container: null
+  };
+
+  var jqueryMap = {};
+
+  var setJqueryMap, initModule;
+
+  setJqueryMap = function(){
+    var $container = stateMap.$container;
+    jqueryMap = {$container: $container};
+  };
+  initModule = function($container){
+    stateMap.$container = $container;
+    $container.html(configMap.main_html);
+    setJqueryMap();
+  }
+
+  return {initModule: initModule};
+}());
